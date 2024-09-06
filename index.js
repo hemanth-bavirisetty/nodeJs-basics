6e4e74c52902f555630de05c618a484e2c377764
Bavirisetty Hemanth Manikanta <72919457+hemanth-bavirisetty@users.noreply.github.com>
2024-09-06 23:28:10 +0530

    configured server using http

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const http = require('http')
 
+const server = http.createServer((req, res) => {
+
+    res.writeHead(200, {
+         'Content-Type': 'text/plain' 
+        }
+    )
+    res.write('Hello World\n')
+
+    res.end()
+}).listen(3001)
+console.log('Server running at http://localhost:3001')
